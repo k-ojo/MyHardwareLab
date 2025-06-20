1637bbcd198eb4ade0ee952e3ca04d2f6eb8ac78
kojo <gideonad4@gmail.com>
2025-06-20 16:44:49 +0000

    added ignore

---
+++ verilog/and_gate_tb.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+module and_gate_tb;
+    reg a, b;
+    wire y;
+
+    and_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        $dumpfile("wave.vcd"); // for GTKWave
+        $dumpvars(0, and_gate_tb);
+
+        a = 0; b = 0;
+        #10 a = 0; b = 1;
+        #10 a = 1; b = 0;
+        #10 a = 1; b = 1;
+        #10 $finish;
+    end
+endmodule
